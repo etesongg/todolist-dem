ac2917b37d04fedb3183cd094dfc28749c2fa5ff
etesongg <etesong0114@gmail.com>
2024-10-17 00:07:43 +0900

    mongodb, cors setting

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
-
+require("dotenv").config();
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter); // /api/tasks -> url에 /api로 불리면 indexRouter로 감
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
 
+const mongoURI = MONGODB_URI_PROD;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true }) // 구버전 url을 사용하더라도 연결이 잘 되게 설정
   .then(() => console.log(`mongoose connected`))
