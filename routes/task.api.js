b953cb25bfeca3fc42b9627f38a725a1d5a233aa
etesongg <etesong0114@gmail.com>
2024-10-20 23:06:25 +0900

    게시판에 작성자를 보여주기 - 모델에 author 추가 후 외래키 설정 - populate 사용 - 미들웨어 설정

--- routes/task.api.js
+++ routes/task.api.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const taskController = require("../controller/task.controller");
+const authController = require("../controller/auth.controller");
 
 // /tasks
-router.post("/", taskController.createTask);
+router.post("/", authController.authenticate, taskController.createTask);
 router.get("/", taskController.getTask);
 router.put("/:id", taskController.updateTask);
 router.delete("/:id", taskController.deleteTask);
